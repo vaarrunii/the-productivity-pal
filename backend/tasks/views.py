7484a0dad9c14300c72134fa158ce6c9ddf4be9d
Varuni <varunihuddar@gmail.com>
2025-05-22 15:56:34 +0530

    🌸 Initial commit of The Productivity Pal

---
+++ backend/tasks/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+
+# Create your views here.
+from rest_framework import viewsets
+from .models import Task
+from .serializers import TaskSerializer
+
+class TaskViewSet(viewsets.ModelViewSet):
+    queryset = Task.objects.all()
+    serializer_class = TaskSerializer
